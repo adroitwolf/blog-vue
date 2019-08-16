2af84e7fd58bfe43c062ec9042518ada4f406a65
whoami <1363531458@qq.com>
2019-08-16 11:59:31 +0800

    Update admin.js

---
+++ src/api/admin.js
@@ -0,0 +1,43 @@
+import service from '@/util/service'
+
+import md5 from 'js-md5'
+
+const adminUrl = '/api/admin'
+
+const adminApi = {}
+
+
+// 登陆函数
+adminApi.login = (username, password) => {
+    return service({
+        url: `${adminUrl}/login`,
+        data: {
+            username: username,
+            password: md5(password)
+        },
+        method: 'post'
+    })
+}
+
+
+adminApi.changePassword = (opassword, password) => {
+    return service({
+        url: `${adminUrl}/changePassword`,
+        data: {
+            oldPassword: md5(opassword),
+            newPassword: md5(password)
+
+        },
+        method: 'put'
+    })
+}
+
+
+adminApi.logout = () => {
+    return service({
+        url: `${adminUrl}/logout`,
+        method: 'post'
+    })
+}
+
+export default adminApi
