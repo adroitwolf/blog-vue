1ea0267f1b7b34c13f2c79885c7d7c4ee078fe4c
whoami <1363531458@qq.com>
2020-03-03 23:55:03 +0800

    增加评论管理模块

--- src/config/router.config.js
+++ src/config/router.config.js
@@ -32,67 +32,76 @@ export const asyncRouterMap = [{
         import ('@/views/manage/index'),
     redirect: '/admin/index.html/status',
     children: [{
-        path: 'status',
-        name: '状态面板',
-        meta: { Bread: ["主页", "状态面板"], icon: 'md-desktop', title: "后台管理", role: 'USER' },
-        component: () =>
-            import ('@/views/manage/components/manager-status')
-    }, { //管理员菜单
-        path: 'check',
-        name: '审核',
-        meta: { Bread: ["主页", "审核"], title: '后台管理', role: 'ADMIN', icon: 'md-people' },
-        component: component,
-        children: [{
-            path: 'user',
-            meta: { Bread: ["主页", "审核", '用户管理'], title: '后台管理' },
-            name: '用户管理',
+            path: 'status',
+            name: '状态面板',
+            meta: { Bread: ["主页", "状态面板"], icon: 'ios-speedometer', title: "后台管理", role: 'USER' },
             component: () =>
-                import ('@/views/manage/components/manager-user')
-        }, {
-            path: 'post',
-            meta: { Bread: ["主页", "审核", '文章审核'], title: '后台管理' },
-            name: '文章审核',
-            component: () =>
-                import ('@/views/manage/components/manager-check-article')
-        }],
+                import ('@/views/manage/components/manager-status')
+        }, { //管理员菜单
+            path: 'check',
+            name: '审核',
+            meta: { Bread: ["主页", "审核"], title: '后台管理', role: 'ADMIN', icon: 'md-people' },
+            component: component,
+            children: [{
+                path: 'user',
+                meta: { Bread: ["主页", "审核", '用户管理'], title: '后台管理' },
+                name: '用户管理',
+                component: () =>
+                    import ('@/views/manage/components/manager-user')
+            }, {
+                path: 'post',
+                meta: { Bread: ["主页", "审核", '文章审核'], title: '后台管理' },
+                name: '文章审核',
+                component: () =>
+                    import ('@/views/manage/components/manager-check-article')
+            }],
 
-    }, {
-        path: 'sys',
-        name: '系统设置',
-        meta: { Bread: ['主页', '系统设置'], title: '后台管理', role: 'ADMIN', icon: 'md-settings' },
-        component: () =>
-            import ('@/views/manage/components/manager-system')
-    }, {
-        path: 'article',
-        meta: { Bread: ["主页", "文章"], icon: 'ios-chatboxes', title: "后台管理", role: 'USER' },
-        name: '文章',
-        component: component,
-        children: [{
-            path: 'manager',
-            name: '所有文章',
-            meta: { Bread: ["主页", "文章", '所有文章'], title: "后台管理" },
-            component: () =>
-                import ('@/views/manage/components/manager-article')
         }, {
-            path: 'write',
-            meta: { Bread: ["主页", "文章", "写文章"], title: "后台管理" },
-            name: '写文章',
+            path: 'sys',
+            name: '系统设置',
+            meta: { Bread: ['主页', '系统设置'], title: '后台管理', role: 'ADMIN', icon: 'md-settings' },
             component: () =>
-                import ("@/views/manage/components/manager-article-write")
-        }],
-    }, {
-        path: 'photo',
-        name: '附件',
-        meta: { Bread: ["主页", "附件"], title: "后台管理", icon: 'ios-albums-outline', role: 'USER' },
-        component: () =>
-            import ("@/views/manage/components/manager-photo")
-    }, {
-        path: 'userProfile',
-        name: '个性管理',
-        meta: { Bread: ["主页", "用户", "个人资料"], title: "后台管理", icon: 'ios-person-outline', role: 'USER' },
-        component: () =>
-            import ('@/views/manage/components/manager-user-profile')
-    }],
+                import ('@/views/manage/components/manager-system')
+        }, {
+            path: 'article',
+            meta: { Bread: ["主页", "文章"], icon: 'ios-chatboxes', title: "后台管理", role: 'USER' },
+            name: '文章',
+            component: component,
+            children: [{
+                path: 'manager',
+                name: '所有文章',
+                meta: { Bread: ["主页", "文章", '所有文章'], title: "后台管理" },
+                component: () =>
+                    import ('@/views/manage/components/manager-article')
+            }, {
+                path: 'write',
+                meta: { Bread: ["主页", "文章", "写文章"], title: "后台管理" },
+                name: '写文章',
+                component: () =>
+                    import ("@/views/manage/components/manager-article-write")
+            }],
+        },
+        {
+            path: 'comment',
+            meta: { Bread: ["主页", "评论管理"], icon: 'ios-photos', title: '评论管理', role: 'USER' },
+            name: '评论管理',
+            component: () =>
+                import ("@/views/manage/components/manager-comment")
+
+        }, {
+            path: 'photo',
+            name: '附件',
+            meta: { Bread: ["主页", "附件"], title: "后台管理", icon: 'ios-albums-outline', role: 'USER' },
+            component: () =>
+                import ("@/views/manage/components/manager-photo")
+        }, {
+            path: 'userProfile',
+            name: '个性管理',
+            meta: { Bread: ["主页", "用户", "个人资料"], title: "后台管理", icon: 'ios-person-outline', role: 'USER' },
+            component: () =>
+                import ('@/views/manage/components/manager-user-profile')
+        }
+    ],
 }, {
     path: '*',
     meta: { title: '404-战术小队', requiresAuth: false, hideInMenu: true },
--- src/views/manage/components/manager-comment.vue
+++ src/views/manage/components/manager-comment.vue
@@ -1,18 +1,282 @@
 <template>
-    
+  <div>
+    <Card>
+      <div class="wrap">
+        <div class="commentList">
+          <div class="comment-item" v-for="(comment,index) in commentsList" :key="index">
+            <div class="user-avatar">
+              <img :src="comment.self.user.avatar?comment.self.user.avatar:avatarUrl" />
+            </div>
+            <div class="article-wrap">
+              <router-link class="pic" :to="{name:'博客内容', query:{id: comment.blog.id}}">
+                <img
+                  :src="comment.blog.picture?comment.blog.picture:imgList[comment.blog.id/3%imgList.length]"
+                />
+                <span class="article-title">{{comment.blog.title}}</span>
+              </router-link>
+            </div>
+            <div class="ci-title">
+              <span>{{comment.self.user.nickname}}</span>
+              <span class="ci-title-split" v-if="comment.parent">回复</span>
+              <span v-if="comment.parent">{{comment.parent.user.nickname}}</span>
+              <span class="show-parent" :id="'parent_'+index" v-if="comment.parent">
+                <span
+                @click="ctrlParentComment(index,'block')"
+                class="open action" style="display:inline"
+              >查看评论</span>
+              <span class="close action" style="display:none" @click="ctrlParentComment(index,'none')">收起评论</span>
+              <div class="ci-parent-reply" v-if="comment.parent" style="display:none;">
+                <div class="cipr-avatar" >
+                  <!-- {{comment.parent.id}} -->
+                  <img :src="comment.parent.user.avatar?comment.parent.user.avatar:avatarUrl" />
+                </div>
+                <div class="cipr-head">
+                  <span>{{comment.parent.user.nickname}}</span>
+                  <div class="cipr-content">{{comment.parent.content}}</div>
+                  <div class="cipr-footer">{{comment.parent.createTime}}</div>
+                </div>
+              </div>
+              </span>
+            </div>
+            <div class="ci-content">{{comment.self.content}}</div>
+            <div class="ci-action">
+              <span class="data">{{comment.self.createTime}}</span>
+              <span class="split">|</span>
+              <span class="delete" @click="deleteComment(comment.self.id)">删除</span>
+            </div>
+          </div>
+        </div>
+        <div class="page">
+          <Page
+            :total="pageInfo.total"
+            :current="pageInfo.pageNum"
+            @on-change="changepage"
+            @on-page-size-change="_nowPageSize"
+            :page-size-opts="pageInfo.page"
+            show-total
+            show-sizer
+            :page-size="pageInfo.pageSize"
+          ></Page>
+        </div>
+      </div>
+    </Card>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: '',
-        data() {
-            return {
-                
-            }
-        }
+import $ from "jquery";
+import commentApi from "@/api/comment";
+import { mapGetters } from "vuex";
+import { Card, Page,Modal,Message } from "view-design";
+export default {
+  name: "commentManage",
+  components: {
+    Card,
+    Page,
+    Modal,
+    Message
+  },
+  data() {
+    return {
+      commentsList: [],
+      pageInfo: {
+        pageSize: 5,
+        pageNum: 1,
+        total: 0,
+        page: [5, 10, 20, 50],
+      }
+    };
+  },
+  computed: {
+    ...mapGetters(["imgList", "baseAvatar"])
+  },
+  methods: {
+    getList() {
+      commentApi.manage(this.pageInfo).then(res => {
+        let data = res.data;
+        this.commentsList = data.rows;
+        this.pageInfo.total = data.total;
+      });
+    },
+    ctrlParentComment(index,css) {
+      const content  = "#parent_" + index + " .ci-parent-reply";
+      const close = "#parent_" + index + " .close";
+      const open = "#parent_" + index + " .open";
+      if(css === 'none'){ //说明此时点击的是收回评论
+        $(close).css("display",css);
+        $(open).css("display","inline");
+      }else{
+        $(close).css("display","inline");
+        $(open).css("display","none");
+      }
+      $(content).css("display",css);
+    },
+    deleteComment(id){
+      let that = this;
+      Modal.confirm({
+              title: "是否删除该评论？",
+              content: "该操作不可逆",
+              onOk: () => {
+                commentApi.delete(id).then(res=>{
+                  that.pageInfo.PageSize = 5;
+                  that.pageInfo.pageNum = 1;
+                  Message.success("成功删除评论");
+                  that.getList();
+                })
+              }
+            });
+    },
+    //点击，切换页面
+    changepage(index) {
+      this.pageInfo.pageNum = index;
+      this.getList();
+    },
+    //每页显示的数据条数
+    _nowPageSize(size) {
+      this.loading = true;
+      this.pageInfo.pageSize = size;
+      //实时获取当前需要显示的条数
+      this.getList();
     }
+  },
+  created() {
+    this.getList();
+  }
+};
 </script>
 
-<style lang="" scoped>
+<style scoped>
+
+.wrap{
+  margin: 0px 30px;
+}
+.commentList {
+  margin-top: 10px;
+}
+
+.comment-item {
+  position: relative;
+  padding: 30px 175px 30px 125px;
+  border-bottom: 1px solid #f1f3f7;
+}
+
+.user-avatar {
+  position: absolute;
+  left: 54px;
+  top: 30px;
+  width: 48px;
+  height: 48px;
+  display: inline-block;
+}
+
+.user-avatar img {
+  width: 48px;
+  height: 48px;
+  display: inline-block;
+  border-radius: 48px;
+}
+
+.article-wrap {
+  position: absolute;
+  right: 20px;
+  top: 30px;
+  width: 100px;
+  text-align: center;
+}
+
+.article-wrap .pic img {
+  display: block;
+  width: 100px;
+  height: 60px;
+  margin-bottom: 10px;
+  border-radius: 4px;
+}
+
+.article-wrap .article-title {
+  width: 100px;
+  display: inline-block;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+
+.ci-title span {
+  color: #222;
+  font-weight: 700;
+}
+
+.ci-title .ci-title-split {
+  margin-bottom: 12px;
+  color: #99a2aa;
+}
+
+.ci-title .show-parent .action{
+  color: #00a1d6;
+  cursor: pointer;
+  margin-left: 25px;
+}
+
+.ci-title .ci-parent-reply{
+  background-color: #f1f3f7;
+    border: 1px solid #e5e9ef;
+    border-radius: 4px;
+    padding: 20px 20px 20px 55px;
+    margin-bottom: 20px;
+    position: relative;
+}
+
+
+.ci-parent-reply .cipr-avatar{
     
+    position: absolute;
+    top: 20px;
+    left: 20px;
+}
+
+.cipr-avatar img{
+  width: 24px;
+    height: 24px;
+    border-radius: 24px;
+}
+.ci-parent-reply .cipr-head{
+  line-height: 1;
+  margin-bottom: 10px;
+  color: #222;
+    font-weight: 700;
+}
+
+.cipr-content{
+  font-size: 14px;
+    line-height: 24px;
+    color: #6d757a;
+    margin-bottom: 10px;
+    word-wrap: break-word;
+    word-break: break-all;
+}
+
+.cipr-footer{
+  color: #99a2aa;
+}
+.ci-content {
+  margin-bottom: 12px;
+  line-height: 24px;
+  min-height: 1px;
+  color: #6d757a;
+  font-size: 14px;
+  word-wrap: break-word;
+}
+
+.ci-action{
+  color: #99a2aa;
+}
+
+.ci-action .split{
+      color: #e5e9ef;
+    margin: 0 10px;
+}
+
+
+.page{
+  margin-top:10px;
+}
 </style>
